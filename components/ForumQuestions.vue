b4bdae9c15c651ddcbdfd8841100d82b8106e160
pograjak <22073437+pograjak@users.noreply.github.com>
2020-05-01 20:47:01 +0200

    structure components

---
+++ components/ForumQuestions.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Forum here
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ components/PresentationRequest.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>
+      Hello from Presentation Request
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
