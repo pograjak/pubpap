f1f135b12895d25cb131cead855092879ebc8175
tomas2211 <tomas2211@post.cz>
2020-05-02 12:44:11 +0200

    update forum

--- components/ForumQuestions.vue
+++ components/ForumQuestions.vue
@@ -3,7 +3,33 @@
     Forum here
     <br />
     <button @click="btnClick()">Click</button>
+
+
+    <v-list two-line>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Full cross-validation results available?</v-list-item-title>
+          <v-list-item-subtitle>Tim Cook</v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-icon>
+          <v-icon color="indigo">mdi-phone</v-icon>
+        </v-list-item-icon>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Repeatability issues</v-list-item-title>
+          <v-list-item-subtitle>Jiri Matas</v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-icon>
+          <v-icon color="indigo">mdi-phone</v-icon>
+        </v-list-item-icon>
+      </v-list-item>
+    </v-list>
   </div>
+  
 </template>
 
 <script>
