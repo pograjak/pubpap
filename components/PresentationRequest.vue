ec96b33d2b2e326a5c522a77f9661bcfaab09ec3
pograjak <22073437+pograjak@users.noreply.github.com>
2020-05-02 18:36:33 +0200

    hosting and stripe function

--- components/PresentationRequest.vue
+++ components/PresentationRequest.vue
@@ -8,37 +8,60 @@
     <br />
     <v-btn color="#D4AF37">Add me ($1)</v-btn>
 
-    <GChart
-    type="PieChart"
-    :data="chartData"
-    :options="chartOptions"
-  />
-
-
+    <GChart type="PieChart" :data="chartData" :options="chartOptions" />
+    <div>
+      <v-btn color="success" @click="payment()">Donate me coffe</v-btn>
+    </div>
   </div>
 </template>
 
 <script>
+import { loadStripe } from "@stripe/stripe-js";
+
 export default {
-  data () {
+  data() {
     return {
       // Array will be automatically processed with visualization.arrayToDataTable function
       chartData: [
-        ['User type', 'Votes'],
-        ['PhD', 76],
-        ['Profs', 5],
-        ['Postdoc', 17],
-        ['Other', 58]
+        ["User type", "Votes"],
+        ["PhD", 76],
+        ["Profs", 5],
+        ["Postdoc", 17],
+        ["Other", 58]
       ],
       chartOptions: {
         chart: {
-          title: 'Votes',
-          subtitle: '-',
+          title: "Votes",
+          subtitle: "-"
         }
       }
+    };
+  },
+
+  computed: {},
+
+  methods: {
+    async payment() {
+      console.log("Peyment starts");
+
+      const session = await fetch(
+        "https://europe-west1-pubpap-redcute.cloudfunctions.net/payment"
+      );
+
+      const sessionIdc = await session.json();
+
+      console.log(`Session: ${sessionIdc}`);
+
+      const stripe = await loadStripe(
+        "pk_test_WNZA8yZsjGbAEFC0pDAb1UrF00BJUKByPR"
+      );
+
+      const { error } = await stripe.redirectToCheckout({
+        sessionId: sessionIdc.id
+      });
     }
   }
-}
+};
 </script>
 
 <style scoped></style>
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const stripe = require("stripe")("sk_test_QHUsLKmLDRAJbMTNtnhcmNV900HPnjSqdA");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.payment = functions
+  .region("europe-west1")
+  .https.onRequest(async (request, response) => {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+
+      customer_email: "test@test.unihack",
+
+      line_items: [
+        {
+          name: "presentation request",
+          description: "request for XXX presentation",
+          amount: 1000,
+          currency: "czk",
+          quantity: 1
+        }
+      ],
+
+      success_url:
+        "https://pubpap-redcute.web.app/?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: "https://pubpap-redcute.web.app/"
+    });
+
+    response.set("Access-Control-Allow-Origin", "*");
+    response.send(session);
+  });
