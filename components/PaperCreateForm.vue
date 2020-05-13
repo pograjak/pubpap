794b3c113127f7468d02c432d07946dc620aeaee
tomas2211 <tomas2211@post.cz>
2020-05-13 19:03:06 +0200

    Changed to stepper + added checks

--- components/PaperCreateForm.vue
+++ components/PaperCreateForm.vue
@@ -1,92 +1,106 @@
 <template>
   <v-card>
     <v-card-title>Add your paper</v-card-title>
-    <v-card-subtitle
-      class="pb-0"
-    >Create a new page for your paper by fetching data from arXiv or by entering it manually.</v-card-subtitle>
+    <v-card-subtitle>Create a new page for your paper.</v-card-subtitle>
 
-    <v-list class="pt-5 pb-0">
-      <v-list-item>
+    <v-stepper v-model="stepper" vertical>
+      <!-- arXiv link & fetch -->
+      <v-stepper-step :complete="stepper >= 1" step="1">
+        arXiv link
+        <small>Fetch data from arXiv or choose to fill it manually.</small>
+      </v-stepper-step>
+
+      <v-stepper-content step="1">
         <v-text-field
-          name="paperUrl"
           label="Link to arXiv"
           placeholder="https://arxiv.org/abs/2003.14415"
-          ref="paperUrl"
           :disabled="disabled"
           v-model="paperUrl"
-        >
-          <template v-slot:append-outer>
-            <v-btn color="primary" :disabled="disabled" @click="fetchArxiv">fetch data</v-btn>
-          </template>
-        </v-text-field>
-      </v-list-item>
+          :error-messages="textfieldErrors.arxivlink"
+          class="pb-2"
+        ></v-text-field>
+        <v-btn
+          color="primary"
+          :disabled="disabled"
+          :loading="arxiv_loading"
+          @click="fetchArxiv"
+        >fetch</v-btn>
+        <v-btn text :disabled="disabled" @click="stepper = 2">Fill manually</v-btn>
+      </v-stepper-content>
 
-      <v-divider></v-divider>
+      <!-- Fill in fields -->
+      <v-stepper-step :complete="stepper > 2" step="2">Basic information</v-stepper-step>
 
-      <v-card-subtitle>Basic information</v-card-subtitle>
-
-      <v-list-item>
+      <v-stepper-content step="2">
         <v-text-field
           v-model="title"
-          required
           label="Title"
           placeholder="Title of the paper"
+          :rules="[textfieldRules.required]"
+          :error-messages="textfieldErrors.title"
+          @input="textfieldErrors.title = ''"
           :disabled="disabled"
+          class="pb-2"
         ></v-text-field>
-      </v-list-item>
 
-      <v-list-item>
         <v-text-field
           v-model="authors"
-          required
           label="Author(s)"
           placeholder="Mary Smith, John Dee"
+          :rules="[textfieldRules.required]"
+          :error-messages="textfieldErrors.authors"
+          @input="textfieldErrors.authors = ''"
           :disabled="disabled"
+          class="pb-2"
         ></v-text-field>
-      </v-list-item>
 
-      <v-list-item class="py-0">
         <v-textarea
           v-model="summary"
           label="Abstract"
           placeholder="Abstract of the paper"
+          :rules="[textfieldRules.required]"
+          :error-messages="textfieldErrors.abstract"
+          @input="textfieldErrors.abstract = ''"
           :disabled="disabled"
+          class="pb-2"
         ></v-textarea>
-      </v-list-item>
 
-      <v-list-item class="py-0">
         <v-text-field
-          name="gitHubUrl"
-          ref="gitHubUrl"
-          v-model="gitHubUrl"
+          v-model="githublink"
           label="Link to GitHub"
           placeholder="https://github.com/pograjak/pubpap"
+          :error-messages="textfieldErrors.githublink"
           :disabled="disabled"
+          class="pb-2"
         ></v-text-field>
-      </v-list-item>
 
-      <v-divider></v-divider>
+        <v-btn color="primary" @click="checkBasicInfo">Continue</v-btn>
+        <v-btn text :disabled="disabled" @click="stepper = 1">Back</v-btn>
+      </v-stepper-content>
 
-      <!-- <v-card-subtitle>Paper presentation</v-card-subtitle> -->
+      <!-- Online presentation -->
+      <v-stepper-step :complete="stepper > 3" step="3">Online presentation</v-stepper-step>
 
-      <v-list-item>
+      <v-stepper-content step="3">
+        <p
+          class="subtitle-2"
+        >Help others understand your ideas by giving an online presentation!<br>
+        The presentation will take place as an interactive online call. A recording will be freely available on pubpap.</p>
         <v-checkbox
+          class="subtitle-2"
           label="Organize an online presentation"
           :disabled="disabled"
           v-model="organizePresentation"
         ></v-checkbox>
-      </v-list-item>
-
-      <v-list-item class="py-0">
-        <v-container class="pa-0 ma-0">
+        <v-container class="py-0 px-0">
           <v-row>
-            <v-col cols="12" md="6">
+            <v-col cols="12" md="6" class="pb-0">
               <p
                 class="caption"
               >Set the goal for audience size. The presentation will only be organized if the number is reached.</p>
               <v-slider
                 label="Audience goal"
-                :disabled="(disabled | (!organizePresentation))"
+                :disabled="!organizePresentation"
                 class="pa-0 ma-0"
                 v-model="audienceSize"
                 max="100"
@@ -96,8 +110,8 @@
                 <template v-slot:append>
                   <v-text-field
                     v-model="audienceSize"
+                    :disabled="!organizePresentation"
                     class="pa-0 ma-0"
-                    :disabled="(disabled | (!organizePresentation))"
                     single-line
                     type="number"
                     style="width: 60px"
@@ -105,13 +119,13 @@
                 </template>
               </v-slider>
             </v-col>
-            <v-col cols="12" md="6">
+            <v-col cols="12" md="6" class="pb-0">
               <p
                 class="caption"
-              >Set the ticket price. The main goal of ticket purchase is to prove real interest in the presentation.</p>
+              >Set the ticket price. Mind that the main goal of ticket purchase is to prove real interest in the presentation.</p>
               <v-slider
                 label="Ticket price"
-                :disabled="(disabled | (!organizePresentation))"
+                :disabled="!organizePresentation"
                 class="pa-0 ma-0"
                 v-model="bid"
                 max="20"
@@ -121,8 +135,8 @@
                 <template v-slot:append>
                   <v-text-field
                     v-model="bid"
+                    :disabled="!organizePresentation"
                     class="pa-0 ma-0"
-                    :disabled="(disabled | (!organizePresentation))"
                     single-line
                     type="number"
                     suffix="USD"
@@ -133,26 +147,12 @@
             </v-col>
           </v-row>
         </v-container>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list-item>
-        <v-list-item-content align-center>
-          <div class="text-right py-2">
-            <v-btn color="secondary" @click="disabled = !disabled">Clear</v-btn>
-            <v-btn class="ml-2" color="primary" @click="disabled = !disabled">Submit</v-btn>
-          </div>
-        </v-list-item-content>
-        <!-- <v-container>
-          <v-row no-gutters>
-            <v-col>
-              <v-row justify="end">
-               
-              </v-row>
-            </v-col>
-          </v-row>
-        </v-container>-->
-      </v-list-item>
-    </v-list>
+        <p class="subtitle-2 pb-2">Once the audience goal is reached, we will contact you and the guests with further details.</p>
+
+        <v-btn color="primary" @click="stepper = 4">Submit</v-btn>
+        <v-btn text @click="stepper = 2">Back</v-btn>
+      </v-stepper-content>
+    </v-stepper>
   </v-card>
 </template>
 
@@ -162,52 +162,90 @@ let parseString = require("xml2js").parseString;
 export default {
   data() {
     return {
-      paperUrl: "",
-      data: "",
-      title: "",
-      summary: "",
-      authors: "",
+      // Paper informations
       arxLink: "",
-      gitHubUrl: "",
-      priceGoal: 100,
-      audienceSize: 20,
+      title: "A",
+      authors: "B",
+      summary: "C",
+      githublink: "",
+      organizePresentation: true,
       bid: 5,
+      audienceSize: 20,
+
+      // Page essentials
+      stepper: 1,
+      paperUrl: "",
       disabled: false,
-      organizePresentation: true
+      arxiv_loading: false,
+
+      textfieldErrors: {
+        arxivlink: "",
+        title: "",
+        authors: "",
+        abstract: "",
+        githublink: ""
+      },
+      textfieldRules: {
+        required: value => !!value || "Required."
+      }
     };
   },
 
   methods: {
     async fetchArxiv() {
-      const paperId = /[^/]*$/.exec(this.paperUrl)[0];
+      if (this.paperUrl.length < 1) {
+        this.textfieldErrors.arxivlink =
+          'Enter address or click "Fill manually".';
+        return;
+      }
 
+      this.arxiv_loading = true;
+      this.textfieldErrors.arxivlink = "";
+
+      const paperId = /[^/]*$/.exec(this.paperUrl)[0];
+      console.log(paperId);
       const resp = await fetch(
         // `http://export.arxiv.org/api/query?search_query=${paperId}&start=0&max_results=10`
         `http://export.arxiv.org/api/query?id_list=${paperId}&sortBy=lastUpdatedDate&sortOrder=descending
 &start=0&max_results=1`
       );
 
-      this.data = await resp.text();
+      var data = await resp.text();
 
-      parseString(this.data, (err, result) => {
-        console.log(result.feed.entry[0]);
-        this.title = result.feed.entry[0].title[0]
-          .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines & double spaces
-          .replace(/\s+/g, " ")
-          .trim();
-        this.summary = result.feed.entry[0].summary[0]
-          .replace(/(\r\n|\n|\r)/gm, " ")
-          .replace(/\s+/g, " ")
-          .trim();
+      parseString(data, (err, result) => {
+        // console.log(err);
+        // Fill in variables
+        try {
+          if (result.feed.entry[0].title[0] == "Error") {
+            console.log(result.feed.entry[0]);
+            throw "Arxiv responded with error";
+          }
 
-        var auth_arr = [];
-        for (const i in result.feed.entry[0].author) {
-          console.log(result.feed.entry[0].author[i].name);
-          auth_arr.push(result.feed.entry[0].author[i].name);
+          this.title = result.feed.entry[0].title[0]
+            .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines & double spaces
+            .replace(/\s+/g, " ")
+            .trim();
+          this.summary = result.feed.entry[0].summary[0]
+            .replace(/(\r\n|\n|\r)/gm, " ")
+            .replace(/\s+/g, " ")
+            .trim();
+
+          var auth_arr = []; // authors
+          for (const i in result.feed.entry[0].author) {
+            // console.log(result.feed.entry[0].author[i].name);
+            auth_arr.push(result.feed.entry[0].author[i].name);
+          }
+          this.authors = auth_arr.join(", ");
+          this.arxLink = result.feed.entry[0].id;
+
+          // Control UI
+          this.arxiv_loading = false;
+          this.stepper = 2;
+        } catch (err) {
+          console.log(err);
+          this.textfieldErrors.arxivlink = "Error loading from arXiv.";
+          this.arxiv_loading = false;
         }
-        this.authors = auth_arr.join(", ");
-        // this.authors = ;
-        this.arxLink = result.feed.entry[0].id;
       });
     },
 
@@ -221,13 +259,58 @@ export default {
           authors: this.authors,
           arxLink: this.arxLink,
           summary: this.summary,
-          priceGoal: this.priceGoal,
+          githublink: this.githublink,
+          audienceSize: this.audienceSize,
           bid: this.bid
         });
       console.log(a);
+    },
+    checkBasicInfo() {  // Check the basic infor about paper
+      var error = false;
+      if (this.title.length < 1) {
+        this.textfieldErrors.title = "Required.";
+        error = true;
+      } else {
+        this.textfieldErrors.title = "";
+      }
+
+      if (this.authors.length < 1) {
+        this.textfieldErrors.authors = "Required.";
+        error = true;
+      } else {
+        this.textfieldErrors.authors = "";
+      }
+
+      if (this.summary.length < 1) {
+        this.textfieldErrors.abstract = "Required.";
+        error = true;
+      } else {
+        this.textfieldErrors.abstract = "";
+      }
+
+      // Regex for github link
+      const gitHubRegEx = RegExp(
+        "^https://github.com/[a-z0-9](?:-?[a-z0-9]){0,38}/[a-zA-Z0-9-._]+$"
+      );
+
+      if ((this.githublink.length > 0) & !gitHubRegEx.test(this.githublink)) {
+        this.textfieldErrors.githublink =
+          "Required format: https://github.com/[username]/[repository]";
+        error = true;
+      } else {
+        this.textfieldErrors.githublink = "";
+      }
+
+      if (!error) {
+        this.stepper = 3;
+      }
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.v-stepper{
+    box-shadow: none;
+}
+</style>
