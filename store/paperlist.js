09c85d2c8305c7b24521990f65523ebf8cf18496
tomas2211 <tomas2211@post.cz>
2020-05-18 09:45:30 +0200

    Refactor + recent papers

--- store/paperlist.js
+++ store/paperlist.js
@@ -1,78 +1,108 @@
 import { firestoreAction } from "vuexfire";
 
 export const state = () => ({
-  mypapers: [],
-  subpapers: [],
-  favpapers: []
+  p: {
+    my: [], // own papers
+    sub: [], // subscribed papers
+    fav: [], // favorite papers
+    rec: [] // recent papers
+  }
 });
 
 export const getters = {
-  mypapers: state => state.mypapers,
-  subpapers: state => state.subpapers,
-  favpapers: state => state.favpapers
+  mypapers: state => state.p.my,
+  subpapers: state => state.p.sub,
+  favpapers: state => state.p.fav,
+  recentpapers: state => state.p.rec
 };
 
 export const mutations = {
-  addToMypapers: function(state, pap) {
-    state.mypapers.push(pap);
+  addToList: function(state, { l, pap }) {
+    state.p[l].push(pap);
   },
-  resetMypapers: function(state) {
-    state.mypapers = [];
-  },
-  addToSubpapers: function(state, pap) {
-    state.subpapers.push(pap);
-  },
-  resetSubpapers: function(state) {
-    state.subpapers = [];
-  },
-  addToFavpapers: function(state, pap) {
-    state.favpapers.push(pap);
-  },
-  resetFavpapers: function(state) {
-    state.favpapers = [];
+  resetList: function(state, l) {
+    state.p[l] = [];
   }
+  // addToMypapers: function(state, pap) {
+  //   state.mypapers.push(pap);
+  // },
+  // resetMypapers: function(state) {
+  //   state.mypapers = [];
+  // },
+  // addToSubpapers: function(state, pap) {
+  //   state.subpapers.push(pap);
+  // },
+  // resetSubpapers: function(state) {
+  //   state.subpapers = [];
+  // },
+  // addToFavpapers: function(state, pap) {
+  //   state.favpapers.push(pap);
+  // },
+  // resetFavpapers: function(state) {
+  //   state.favpapers = [];
+  // },
+  // addToRecentpapers: function(state, pap) {
+  //   state.recentpapers.push(pap);
+  // },
+  // resetRecentpapers: function(state) {
+  //   state.recentpapers = [];
+  // }
 };
 
-function loadPapsWithUsr(ctx, arr_name, uid){
+function loadPapsWithUsr(ctx, arr_name, uid) {
   let pRef = ctx.$fireStore
-  .collection("papers")
-  .where(arr_name, "array-contains", uid);
+    .collection("papers")
+    .where(arr_name, "array-contains", uid);
   return pRef.get();
 }
 
 export const actions = {
   loadMyPapers: async function(ctx, userId) {
-    let p;
-    ctx.commit("resetMypapers");
+    ctx.commit("resetList", "my");
 
     let pRef = this.$fireStore
       .collection("papers")
       .where("authorId", "==", userId);
-    p = await pRef.get();
+    let p = await pRef.get();
     p.forEach(function(doc) {
       const dat = doc.data();
       dat.id = doc.id;
-      ctx.commit("addToMypapers", dat);
+      ctx.commit("addToList", { l: "my", pap: dat });
     });
   },
 
   loadSubsPapers: async function(ctx, userId) {
-    ctx.commit("resetSubpapers");
+    ctx.commit("resetList", "sub");
     let p = await loadPapsWithUsr(this, "requestPresentation.subsIds", userId);
     p.forEach(function(doc) {
       let dat = doc.data();
       dat.id = doc.id;
-      ctx.commit("addToSubpapers", dat);
+      ctx.commit("addToList", { l: "sub", pap: dat });
     });
   },
 
   loadFavPapers: async function(ctx, userId) {
-    ctx.commit("resetFavpapers");
+    ctx.commit("resetList", "fav");
     let p = await loadPapsWithUsr(this, "favs", userId);
     p.forEach(function(doc) {
       let dat = doc.data();
       dat.id = doc.id;
-      ctx.commit("addToFavpapers", dat);
+      ctx.commit("addToList", { l: "fav", pap: dat });
     });
   },
+
+  loadRecentPapers: async function(ctx) {
+    ctx.commit("resetList", "rec");
+
+    let pRef = this.$fireStore
+      .collection("papers")
+      .orderBy("createdAt", "desc")
+      .limit(10);
+    let p = await pRef.get();
+    p.forEach(function(doc) {
+      const dat = doc.data();
+      dat.id = doc.id;
+      ctx.commit("addToList", { l: "rec", pap: dat });
+    });
+  }
 };
