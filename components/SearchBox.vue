d771cc525e35bad3540101762e1ab4cd28a2230b
tomas2211 <tomas2211@post.cz>
2020-05-20 12:44:59 +0200

    Added debouncing to reduce # of queries

--- components/SearchBox.vue
+++ components/SearchBox.vue
@@ -1,13 +1,18 @@
 <template>
-  <ais-instant-search index-name="papers" :search-client="searchClient" class="ml-7 d-flex flex-grow-1">
+  <ais-instant-search
+    index-name="papers"
+    :search-client="searchClient"
+    class="ml-7 d-flex flex-grow-1"
+  >
     <v-menu centered max-height="800px" transition="slide-y-transition" offset-y nudge-bottom="2">
       <template v-slot:activator="{ on }">
         <ais-search-box class="d-flex flex-grow-1">
-          <template slot="default" slot-scope="{ currentRefinement, isSearchStalled, refine }">
+          <!-- v-on:input="refine(textInput)" -->
+          <template v-slot="item">
             <v-text-field
-              :value="currentRefinement"
+              :value="item.currentRefinement"
               v-model="textInput"
-              v-on:input="refine(textInput)"
+              v-on:input="typingText(item.refine)"
               class="mr-3"
               v-on="on"
               placeholder="Search"
@@ -21,11 +26,16 @@
 
       <ais-hits>
         <v-list two-line class="py-0" slot-scope="{ items }">
-          <template v-if="items.length == 0">
-            <v-list-item
-              class="py-0"
-              style="min-height: 40px"
-            >
+          <v-list-item class="py-0" style="min-height: 40px" v-if="showSearching">
+            <v-list-item-content class="py-0 text-center">
+              <v-list-item-title>
+                <LoadingSpinner />
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <template v-if="!showSearching && items.length == 0">
+            <v-list-item class="py-0" style="min-height: 40px">
               <v-list-item-content class="py-0 text-center">
                 <v-list-item-title>
                   <v-icon>mdi-emoticon-sad-outline</v-icon>&nbsp;No results
@@ -33,27 +43,30 @@
               </v-list-item-content>
             </v-list-item>
           </template>
-          <template v-for="item in items">
-            <v-list-item
-              :key="item.objectID"
-              class="py-1"
-              style="min-height: 45px"
-              @click="textInput = ''"
-              :to="'/paper/'+item.objectID"
-            >
-              <v-list-item-content class="py-0">
-                <v-list-item-title>
-                  <ais-highlight :hit="item" attribute="title"></ais-highlight>
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                  <ais-highlight :hit="item" attribute="authors"></ais-highlight>
-                </v-list-item-subtitle>
-                <!-- <v-list-item-subtitle>
+
+          <template v-if="!showSearching">
+            <template v-for="item in items">
+              <v-list-item
+                :key="item.objectID"
+                class="py-1"
+                style="min-height: 45px"
+                @click="textInput = ''"
+                :to="'/paper/'+item.objectID"
+              >
+                <v-list-item-content class="py-0">
+                  <v-list-item-title>
+                    <ais-highlight :hit="item" attribute="title"></ais-highlight>
+                  </v-list-item-title>
+                  <v-list-item-subtitle>
+                    <ais-highlight :hit="item" attribute="authors"></ais-highlight>
+                  </v-list-item-subtitle>
+                  <!-- <v-list-item-subtitle>
                   <ais-snippet :hit="item" attribute="summary"></ais-snippet>
-                </v-list-item-subtitle>-->
-              </v-list-item-content>
-            </v-list-item>
-            <v-divider :key="item.objectID+'_'"></v-divider>
+                  </v-list-item-subtitle>-->
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider :key="item.objectID+'_'"></v-divider>
+            </template>
           </template>
         </v-list>
       </ais-hits>
@@ -64,19 +77,55 @@
 
 <script>
 import Vue from "vue";
+import debounce from "lodash/debounce";
 import algoliasearch from "algoliasearch/lite";
 
 import InstantSearch from "vue-instantsearch";
+
+import LoadingSpinner from "~/components/LoadingSpinner.vue";
 Vue.use(InstantSearch);
 
 let algolia = require("~/plugins/credentials.js").algolia;
 
+const algoliaClient = algoliasearch(algolia.appId, algolia.apiKey);
+
 export default {
   data() {
     return {
-      searchClient: algoliasearch(algolia.appId, algolia.apiKey),
-      textInput: ""
+      searchClient: null,
+      textInput: "",
+      showSearching: false
     };
+  },
+  components: {
+    LoadingSpinner
+  },
+  created() {
+    this.searchClient = { search: this.search }; // own search function for proper debounce
+  },
+  methods: {
+    async search(requests) {
+      // console.log("Search invoke");
+      let hits = await algoliaClient.search(requests);
+      this.showSearching = false;
+      return hits;
+    },
+    debounceInput: debounce(
+      function(refineFcn) {
+        // console.log(`Search: ${this.textInput}`);
+        refineFcn(this.textInput);
+      },
+      800, // debouce delay
+      { maxWait: 2000 }
+    ),
+    typingText(refineFcn) {
+      this.showSearching = true;
+      if (this.textInput.length > 0) {
+        this.debounceInput(refineFcn); // debounce input
+      } else {
+        refineFcn(this.textInput); // call straight away on empty query
+      }
+    }
   }
 };
 </script>
