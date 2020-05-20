4e1615fc3f4bef4392860bb232d274f3fbd3d3c7
tomas2211 <tomas2211@post.cz>
2020-05-20 22:22:44 +0200

    Added paperId to paper properties

--- store/paper.js
+++ store/paper.js
@@ -10,7 +10,7 @@ export const getters = {
   imgUrl: state => state.imgUrl,
   requestPresentation: state => state.paper.requestPresentation,
   isPresentation: state => {
-    if(state.paper && state.paper.requestPresentation.bid){
+    if (state.paper && state.paper.requestPresentation.bid) {
       return true;
     }
     return false;
@@ -18,9 +18,10 @@ export const getters = {
 };
 
 export const mutations = {
-  loadPaper: function(state, paper) {
+  loadPaper: function(state, { paper, paperId }) {
     // console.log(paper);
     state.paper = paper;
+    state.paper["paperId"] = paperId;
   },
   setImgUrl: function(state, url) {
     state.imgUrl = url;
@@ -46,7 +47,7 @@ export const actions = {
       return false;
     }
 
-    ctx.commit("loadPaper", p.data());
+    ctx.commit("loadPaper", { paper: p.data(), paperId: paperId });
     return true; // HAS TO RETURN TRUE for nuxt validate in pages/paper/_id.vue
   },
 
@@ -58,7 +59,7 @@ export const actions = {
     meta = await this.$fireStorage.ref(imgName).getMetadata();
 
     ctx.commit("setImgUrl", url);
-    return {url: url, meta: meta};
+    return { url: url, meta: meta };
   },
 
   editFavorites: async function({ commit, state }, paperId) {
