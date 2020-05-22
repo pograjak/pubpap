4167277a2d4f3821a73cacce1d5a1f1a739ef44e
tomas2211 <tomas2211@post.cz>
2020-05-22 17:55:00 +0200

    New proposal for about

--- pages/about.vue
+++ pages/about.vue
@@ -14,12 +14,8 @@
       <v-icon>mdi-arrow-up</v-icon>
     </v-btn>
 
-    <section id="hero">
+    <section>
       <v-row no-gutters>
-        <!-- <v-img
-            :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
-            src="https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80"
-        >-->
         <div
           :style="'min-height: calc(100vh - ' + $vuetify.application.top + 'px); min-width: 100%'"
         >
@@ -31,19 +27,17 @@
                     :class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']"
                     class="font-weight-black"
                   >pubpap</div>
-                  <!-- <br /> -->
                   <div
                     :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
                     class="font-weight-light pt-6"
                   >where asking is gaining</div>
                 </v-col>
-
                 <v-btn
                   color="black"
                   class="align-self-end mt-8"
                   fab
                   outlined
-                  @click="$vuetify.goTo('#about-me')"
+                  @click="$vuetify.goTo('#about')"
                 >
                   <v-icon>mdi-chevron-double-down</v-icon>
                 </v-btn>
@@ -51,19 +45,17 @@
             </v-container>
           </v-theme-provider>
         </div>
-        <!-- </v-img> -->
       </v-row>
     </section>
 
-    <!-- <section
-      id="about-me"
-      :style="'min-height: calc(100vh - ' + $vuetify.application.top + 'px); min-width: 100%'"
-    >-->
-    <section id="about-me">
+    <section id="about">
       <div class="py-12"></div>
 
       <v-container class="text-center">
-        <h2 class="display-2 font-weight-bold mb-3">pubpap</h2>
+        <h2
+          class="font-weight-black mb-8"
+          :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+        >present & discuss research papers</h2>
 
         <v-responsive class="mx-auto mb-8" width="56">
           <v-divider class="mb-1"></v-divider>
@@ -72,62 +64,106 @@
         </v-responsive>
 
         <v-responsive class="mx-auto" max-width="900">
-          <div class="headline mb-8">free online platform for presenting & discussing research</div>
+          <div :class="[$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1']" class="font-weight-bold mb-3">Present your publications</div>
+          <div :class="[$vuetify.breakpoint.smAndDown ? 'title' : 'headline']" class="font-weight-light align-center mb-10">easily set-up online presentations to relevant audience</div>
 
-          <div class="title font-weight-black mb-0">Magnify impact</div>
-          <div class="subtitle-1 align-center mb-4">by presenting online for everyone</div>
-
-          <div class="title font-weight-black mb-0">Collaborate with the right people</div>
-          <div
-            class="subtitle-1 align-center mb-4"
-          >by sharing questions, comments or additional results</div>
-
-          <div class="title font-weight-black mb-0">Get relevant feedback</div>
-          <div class="subtitle-1 align-center">from across the academic community</div>
+          <div :class="[$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1']" class="font-weight-bold mb-3">Discuss with others</div>
+          <div :class="[$vuetify.breakpoint.smAndDown ? 'title' : 'headline']" class="font-weight-light align-center mb-10">publicly share ideas, questions or additional results</div>
         </v-responsive>
       </v-container>
 
       <div class="py-12"></div>
     </section>
 
-    <section id="howto">
+    <section id="how">
       <div class="py-12"></div>
 
       <v-container class="text-center">
-        <h2 class="display-2 font-weight-bold mb-3">How does it work?</h2>
+        <h2
+          class="font-weight-black mb-6"
+          :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+        >How does it work?</h2>
 
-        <v-responsive class="mx-auto" max-width="1000">
+        <v-responsive class="mx-auto" max-width="1300">
           <v-container>
             <v-row>
               <v-col cols="12" md="6">
-                <div class="display-1 font-weight-black mb-6">As an author</div>
-                <ol class="headline px-0" style="list-style-position: inside">
-                  <li>Add your paper & set a goal<p
-                   class="subtitle-1 mb-5">Create a page for your paper. Set the goal size of the audience you'd like to present to. Add a moderate ticket price to make sure they will show up.</p>
+                <div :class="[$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1']" class="font-weight-bold mb-6">As an author</div>
+                <ol :class="[$vuetify.breakpoint.smAndDown ? 'title' : 'headline']" class="px-0  font-weight-bold" style="list-style-position: inside">
+                  <li>
+                    Add your paper
+                    <p :class="[$vuetify.breakpoint.smAndDown ? 'subtitle-1 ' : 'title']"
+                      class="mb-10"
+                    >
+                    Create a page for your paper on pubpap. Set the minimal size of the audience you'd like to present to. Add a moderate ticket price to prove they are interested.</p>
                   </li>
-                  <li>Present online<p
-                  class="subtitle-1 mb-5">Once all tickets are sold, you will get the chance to show your work to all researches tackling the same problem. Afterward, you get 90% of the collected money.</p>
+                  <li>
+                    Present
+                    <p :class="[$vuetify.breakpoint.smAndDown ? 'subtitle-1 ' : 'title']"
+                      class="mb-10"
+                    >Once all tickets are sold, you will get the chance to show your work to all researches tackling the same problem. Afterward, you get 90% of the collected money.</p>
                   </li>
-                  <li>Share<p
-                  class="subtitle-1 mb-5">A recording will be made publicly available for free. You will thus get a chance to promote your research even more.</p>
+                  <li>
+                    Share
+                    <p :class="[$vuetify.breakpoint.smAndDown ? 'subtitle-1 ' : 'title']"
+                      class="mb-10"
+                    >A recording will be made publicly available for free. You will thus get a chance to promote your research even more.</p>
                   </li>
                 </ol>
               </v-col>
               <v-col cols="12" md="6">
-                <div class="display-1 font-weight-black mb-6">As a user</div>
-                <ol class="headline px-0" style="list-style-position: inside">
-                  <li>Show interest<p 
-                  class="subtitle-1 mb-5">Show the author you are interested in their research by purchasing a ticket for their live presentation.</p>
+                 <div :class="[$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1']" class="font-weight-bold mb-6">As a user</div>
+                <ol :class="[$vuetify.breakpoint.smAndDown ? 'title' : 'headline']" class="px-0  font-weight-bold" style="list-style-position: inside">
+                  <li>
+                    Show interest
+                    <p :class="[$vuetify.breakpoint.smAndDown ? 'subtitle-1 ' : 'title']" class="mb-10">
+                      Show the author you are interested in their publication by purchasing a ticket for their live presentation.</p>
                   </li>
-                  <li>Join the presentation<p
-                  class="subtitle-1 mb-5">You will get the chance to join the presentation and talk to the author once enough people gather.<br/>If a paper does not get enough interest, your money will be refunded.</p>
+                  <li>
+                    Join the presentation
+                    <p :class="[$vuetify.breakpoint.smAndDown ? 'subtitle-1 ' : 'title']" class="mb-10">
+                      You will get the chance to join the presentation and talk to the author once enough people gather*.
+                    </p>
                   </li>
                 </ol>
+                
+                <div class="text-center subtitle-2 mt-12">*Don't worry, if a paper does not get enough interest, your money will be refunded.</div>
               </v-col>
             </v-row>
           </v-container>
-          <div class="display-1 font-weight-black mt-3">plus everyone: discuss!</div>
-          <div class="title mt-2">Every paper page has discussion threads.</div>
+          
+          <!-- <div class="title mt-2">Every paper page has discussion threads.</div> -->
+        </v-responsive>
+      </v-container>
+
+      <div class="py-12"></div>
+    </section>
+
+
+    <section id="why">
+      <div class="py-12"></div>
+
+      <v-container class="text-center">
+        <h2
+          class="font-weight-black mb-8"
+          :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+        >Why?</h2>
+
+        <v-responsive class="mx-auto mb-8" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-responsive class="mx-auto" max-width="900">
+          <div :class="[$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1']" class="font-weight-bold mb-3">Magnify impact</div>
+          <div :class="[$vuetify.breakpoint.smAndDown ? 'title' : 'headline']" class="font-weight-light align-center mb-10">by presenting online for everyone</div>
+
+          <div :class="[$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1']" class="font-weight-bold mb-3">Collaborate with the whole community</div>
+          <div :class="[$vuetify.breakpoint.smAndDown ? 'title' : 'headline']" class="font-weight-light align-center mb-10">by sharing questions, comments or additional results</div>
+
+          <div :class="[$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1']" class="font-weight-bold mb-3">Get relevant feedback</div>
+          <div :class="[$vuetify.breakpoint.smAndDown ? 'title' : 'headline']" class="font-weight-light align-center mb-10">from across the academic community</div>
         </v-responsive>
       </v-container>
 
@@ -143,7 +179,6 @@
         <v-responsive class="mx-auto title font-weight-light mb-8" max-width="720">
           <v-container>
             <v-row>
-              
               <v-col>
                 <a href="https://www.linkedin.com/in/tomas-novak/" target="_blank">
                   <v-avatar class="elevation-12 mb-6" size="128">
@@ -153,7 +188,7 @@
                 <div class="headline font-weight-medium">Tomas Novak</div>
                 <!-- <div class="caption">Co-founder</div> -->
               </v-col>
-              
+
               <v-col>
                 <a href="https://www.linkedin.com/in/jakub-pogran/" target="_blank">
                   <v-avatar class="elevation-12 mb-6" size="128">
