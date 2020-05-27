328f6a962905272e8e87d9fa1573c773cea59710
tomas2211 <tomas2211@post.cz>
2020-05-27 21:58:23 +0200

    Moved edit paper icons to a separate component

---
+++ components/EditPaperIcons.vue
@@ -0,0 +1,160 @@
+<template>
+  <div>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          icon
+          large
+          v-on="on"
+          @click.prevent="openThumbDiag"
+          @mousedown.stop
+          @touchstart.native.stop
+        >
+          <v-icon>mdi-image-edit</v-icon>
+        </v-btn>
+      </template>
+      <span>Change thumbnail</span>
+    </v-tooltip>
+
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          v-on="on"
+          icon
+          large
+          @click.prevent="deleteDialog = true"
+          @mousedown.stop
+          @touchstart.native.stop
+        >
+          <v-icon>mdi-delete</v-icon>
+        </v-btn>
+      </template>
+      <span>Request removal</span>
+    </v-tooltip>
+
+    <!-- Delete dialog -->
+    <v-dialog v-model="deleteDialog" max-width="400px">
+      <v-card>
+        <v-card-title class="title font-weight-bold">Delete Paper</v-card-title>
+
+        <v-card-text class="error--text text--darken-1 red lighten-4 py-4">
+          Do you really want to delete the whole paper
+          <strong>discussion</strong> and
+          <strong>presentation requests?</strong>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="deleteDialog = false">Cancel</v-btn>
+          <!-- TODO: Change to the support email -->
+          <v-btn
+            color="error"
+            error
+            :href="`mailto:tomas@redcute.cz, jakub@redcute.cz?subject=[pubpap] Please delete paper ID \'${paper.id}\'&body=Dear pubpap team,%0D%0A%0D%0AI would like to request a removal of paper \'${paper.title}\' - ID: \'${paper.id}\' because of the following reasons:%0D%0A%0D%0A`"
+            @click="deleteDialog = false"
+          >Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- Change Thumbnail dialog -->
+    <v-dialog v-model="thumbnailDialog" max-width="800px">
+      <v-card>
+        <v-card-title>Change thumbnail</v-card-title>
+        <div class="px-6">
+          <ImageUpload
+            ref="imageUploader"
+            :paperId="paper.id"
+            @selected="thumbnailDiagDisable.upload = false"
+            @canceled="thumbnailDiagDisable.upload = true"
+            @loadedRemote="thumbnailDiagDisable.delete = false"
+          />
+        </div>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="cancelThumbDiag">Cancel</v-btn>
+          <v-btn
+            text
+            :loading="thumbnailDiagLoading.delete"
+            :disabled="thumbnailDiagDisable.delete"
+            @click="deleteThumbDiag"
+          >Delete current</v-btn>
+          <v-btn
+            color="primary"
+            :disabled="thumbnailDiagDisable.upload"
+            :loading="thumbnailDiagLoading.upload"
+            @click="uploadThumbDiag"
+          >Upload</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import ImageUpload from "~/components/ImageUpload.vue";
+
+export default {
+  props: {
+    paper: Object
+  },
+  components: {
+    ImageUpload
+  },
+  data() {
+    return {
+      deleteDialog: false,
+      thumbnailDialog: false,
+      thumbnailDiagLoading: {
+        upload: false,
+        delete: false
+      },
+      thumbnailDiagDisable: {
+        upload: true,
+        delete: true
+      }
+    };
+  },
+
+  methods: {
+    openThumbDiag() {
+      this.thumbnailDialog = true;
+      this.thumbnailDiagDisable.upload = true;
+      this.thumbnailDiagDisable.delete = true;
+      if (this.$refs.imageUploader) {
+        this.$refs.imageUploader.reloadRemoteImg();
+      }
+    },
+    cancelThumbDiag() {
+      this.$refs.imageUploader.setupCropper(null);
+      this.thumbnailDialog = false;
+    },
+    deleteThumbDiag() {
+      this.thumbnailDiagLoading.delete = true;
+      this.$store.dispatch("paper/deleteThumbnail", this.paper.id).then(() => {
+        this.thumbnailDiagLoading.delete = false;
+        // this.thumbnailDiagDisable.delete = true;
+        this.thumbnailDialog = false;
+        this.$refs.imageUploader.setupCropper(null);
+      });
+    },
+    uploadThumbDiag() {
+      this.thumbnailDiagLoading.upload = true;
+
+      // Retrieve image
+      const imgobj = this.$refs.imageUploader.getImage();
+
+      this.$store
+        .dispatch("paper/uploadThumbnail", {
+          paperId: this.paper.id,
+          img: imgobj.img
+        })
+        .then(() => {
+          this.thumbnailDiagLoading.upload = false;
+          this.thumbnailDialog = false;
+          this.$refs.imageUploader.setupCropper(null);
+        });
+    }
+  }
+};
+</script>
